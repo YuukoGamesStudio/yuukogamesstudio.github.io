2615e88557295cf81527414bc8a77994fff4b9e7
Aleix Ferré <thecatalahd@gmail.com>
2023-06-17 15:20:19 +0200

    Optimize for seo, passing lighthouse 100

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private meta: Meta) {}
+
+  ngOnInit(): void {
+    this.meta.addTags([
+      {
+        property: 'og:title',
+        content: 'Yuuko Games Studio - Homemade videogames',
+      },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:url', content: 'https://yuukogamesstudio.github.io/' },
+      {
+        property: 'og:image',
+        content: 'https://yuukogamesstudio.github.io/assets/img/logo.png',
+      },
+      {
+        property: 'og:description',
+        content:
+          'Yuuko Games is a two person indie studio based in Girona. We make videogames that use innovative mechanics to create immersive experiences.',
+      },
+      { name: 'theme-color', content: '#bc2e18' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+    ]);
+  }
 }
