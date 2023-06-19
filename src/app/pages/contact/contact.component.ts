ba656f889591f9779a16c0d1540b3022d9a6c581
Aleix Ferré <thecatalahd@gmail.com>
2023-06-19 22:16:09 +0200

    Contact page

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { SOCIALS } from '@shared/footer/socials.model';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
-
+  socials = SOCIALS;
 }
