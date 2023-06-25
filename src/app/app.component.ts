2e68183e697a107eabac96a29d1701b523da5ef2
Aleix Ferré <thecatalahd@gmail.com>
2023-06-25 17:12:49 +0200

    Renaming animations config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Meta } from '@angular/platform-browser';
-import { fadeAnimation } from './animations';
+import { fadeAnimation } from './animations.config';
 
 @Component({
   selector: 'app-root',
